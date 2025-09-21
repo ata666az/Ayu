72e44fb4194cdadfc89749064e5038d4c1bfaa00
ata666az <ata666az@gmail.com>
2025-09-22 03:11:52 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "ayu";
+let APP_DOMAIN = "ata666az.workers.dev";
 
 let prxIP = "";
 let cachedPrxList = [];
